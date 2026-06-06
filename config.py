6fbd8e45f7ed80dfe4c88e4bb1e1a036480bf910
rene802 <154843288+rene802@users.noreply.github.com>
2026-06-06 18:38:19 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+
+
+# Directory structure for results
+base_dir = "results"
+model_dir = os.path.join(base_dir, "models")
+plot_dir = os.path.join(base_dir, "plots")
+root_dir = os.path.join(base_dir, "root")
+log_dir = os.path.join(base_dir, "logs")
+
+for d in [model_dir, plot_dir, root_dir, log_dir]:
+    os.makedirs(d, exist_ok=True)
